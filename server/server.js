999d3e3eaa95deeb68355ef5c4ace56194f7045d
JayWilling <jwilling123@gmail.com>
2024-03-08 14:09:36 +1100

    Increased payload limit
    The initial data transfer of all star data to the database required an increased payload limit. I am uncertain of what an upper limit may be in the future, but it will not need to go higher than 20mb (hopefully).

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+
 app.use(cors());
-app.use(express.json());
-app.use(require("./routes/topic"));
+app.use(express.json({ limit : '20mb'}));
+app.use(express.urlencoded({ limit : '20mb'}));
+app.use(require("./routes/star"));
+
 // get driver connection
 const dbo = require("./db/conn");
 app.listen(port, async () => {
