0ba57bdb040f2cf6d97133d8d1c24cdd4261e46b
JayWilling <jwilling123@gmail.com>
2024-03-26 16:49:06 +1100

    [GREY-33, GREY-6] Cleaned star routes
    References to the old dbo constant were removed to opt for the async/await try/catch structure instead. The code compiles however not all functions have been tested yet. Not all functions are yet in use on the frontend so will be updated as needed. Error codes and messages will be improved if needed as well.

--- server/src/routes/star.ts
+++ server/src/routes/star.ts
@@ -1,20 +1,14 @@
 import express, {Request, Response} from "express";
 // import { ObjectId } from "mongodb";
- 
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
-export const starRoutes = express.Router();
- 
-// This will help us connect to the database
-import dbo from "../db/conn";
+
 import { collections } from "../db/conn";
- 
-// This help convert the id from string to ObjectId for the _id.
+
 // import { ObjectId } from "mongodb";
 const ObjectId = require("mongodb").ObjectId;
- 
+
 // Route Functions
+
+export const starRoutes = express.Router();
 
 // GET
 
@@ -28,75 +22,109 @@ async function getStars(req: Request, res: Response) {
         // res.json(stars);
     } catch (error) {
         console.log(error);
+        res.sendStatus(500);
     }
 }
 
 async function getStarById(req: Request, res: Response) {
-    let db_connect = dbo.getDb();
- let myquery = { _id: ObjectId(req.params.id) };
- db_connect
-   .collection("stars")
-   .findOne(myquery, function (err, result) {
-     if (err) throw err;
-     res.json(result);
-   });
+
+    if (!collections.stars) {
+        res.sendStatus(503);
+    };
+
+    try {
+        const query = { _id: ObjectId(req.params.id)};
+        const star = await collections.stars.findOne(query);
+        res.status(200).send(star);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
 // POST
 
 // Add initial list of default stars
 async function addStarList(request: Request, response: Response) {
-    let db = dbo.getDb();
 
-    db.collection("stars").insertMany(request.body, function (err, res) {
-        if (err) throw err;
-        response.json(res);
-    })
+    if (!collections.stars) {
+        response.sendStatus(503);
+    };
+
+    try {
+        const insertManyResult = await collections.stars.insertMany(request.body);
+        response.status(200).send(insertManyResult);
+    } catch (error) {
+        console.log(error);
+        response.sendStatus(500);
+    }
 }
 
 async function addStar(req: Request, response: Response) {
-    let db_connect = dbo.getDb();
- let myobj = {
-   data: req.body.data,
-   adjacent: req.body.adjacent,
-   children: req.body.children,
-   comparator: req.body.comparator,
- };
- db_connect.collection("stars").insertOne(myobj, function (err, res) {
-   if (err) throw err;
-   response.json(res);
- });
+
+    if (!collections.stars) {
+        response.sendStatus(503);
+    };
+
+    try {
+        const starObj = {
+            data: req.body.data,
+            adjacent: req.body.adjacent,
+            children: req.body.children,
+            comparator: req.body.comparator,
+        };
+        await collections.stars.insertOne(starObj);
+        response.sendStatus(200);
+    } catch (error) {
+        console.log(error);
+        response.sendStatus(500);
+    }
 }
 
 async function updateStar(req: Request, response: Response) {
-    let db_connect = dbo.getDb();
- let myquery = { _id: ObjectId(req.params.id) };
- let newvalues = {
-   $set: {
-     name: req.body.name,
-     position: req.body.position,
-     level: req.body.level,
-   },
- };
- db_connect
-   .collection("stars")
-   .updateOne(myquery, newvalues, function (err, res) {
-     if (err) throw err;
-     console.log("1 document updated");
-     response.json(res);
-   });
+
+    if (!collections.stars) {
+        response.sendStatus(503);
+    };
+
+    try {
+        const query = {
+            _id: ObjectId(req.params.id)
+        };
+        // TODO: Update to use star values
+        const newValues = {
+            $set: {
+                name: req.body.name,
+                position: req.body.position,
+                level: req.body.level,
+              },
+        }
+        await collections.stars.updateOne(query, newValues);
+        response.sendStatus(200);
+    } catch (error) {
+        console.log(error);
+        response.sendStatus(500);
+    }
 }
 
 // DELETE
 
 async function deleteStar(req: Request, res: Response) {
-    let db_connect = dbo.getDb();
- let myquery = { _id: ObjectId(req.params.id) };
- db_connect.collection("stars").deleteOne(myquery, function (err, obj) {
-   if (err) throw err;
-   console.log("1 document deleted");
-   res.json(obj);
- });
+
+    if (!collections.stars) {
+        res.sendStatus(503);
+    };
+
+    try {
+        const query = {
+            _id: ObjectId(req.params.id)
+        };
+        const deletedObject = await collections.stars.deleteOne(query);
+        res.status(200).send(deletedObject);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
 
